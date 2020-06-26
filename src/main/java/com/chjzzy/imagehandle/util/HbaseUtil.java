591f778ea823565f50f28afeeab7a917ca937c4a
ubuntu <124063344@qq.com>
2020-06-26 09:42:31 +0800

    hbase connect

--- src/main/java/com/chjzzy/imagehandle/util/HbaseUtil.java
+++ src/main/java/com/chjzzy/imagehandle/util/HbaseUtil.java
@@ -2,9 +2,9 @@ package com.chjzzy.imagehandle.util;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Admin;
-import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -24,11 +24,63 @@ public class HbaseUtil {
         configuration.set("hbase.rootdir",rootdir);
         connection = ConnectionFactory.createConnection(configuration);
         admin = connection.getAdmin();
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+                try {
+                    if(admin != null){
+                    admin.close();
+                    }
+                    if(null != connection){
+                        connection.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }));
+        //
+        createTable("image","info","statistic");
+    }
+    /*
+     *表名 image
+     *
+     *行键			|	                  列族
+     *		        |     文件信息info   		统计信息statistic
+     *文件名filename	|	图片字节码bytecode 	0-255像素的个数 0,1,2....
+     *
+     *
+     * */
+    private boolean createTable(String name,String... colFamily) throws IOException {
+        TableName tableName=TableName.valueOf(name);
+        if(admin.tableExists(tableName)){
+            return false;
+        }else{
+            TableDescriptorBuilder tableDescriptorBuilder = TableDescriptorBuilder.newBuilder(tableName);
+            for(String str:colFamily){
+                ColumnFamilyDescriptor columnFamilyDescriptor =
+                        ColumnFamilyDescriptorBuilder.newBuilder(Bytes.toBytes(str)).build();
+                tableDescriptorBuilder.setColumnFamily(columnFamilyDescriptor);
+            }
+            admin.createTable(tableDescriptorBuilder.build());
+            return true;
+        }
     }
 
-    private void createTable(){
-
+    public  void insertData(String tableName,String rowKey,String colFamily,String col,String val) throws IOException {
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Put put = new Put(rowKey.getBytes());
+        put.addColumn(colFamily.getBytes(),col.getBytes(), val.getBytes());
+        table.put(put);
+        table.close();
     }
+
+    public  String getData(String tableName,String rowKey,String colFamily, String col)throws  IOException{
+        Table table = connection.getTable(TableName.valueOf(tableName));
+        Get get = new Get(rowKey.getBytes());
+        get.addColumn(colFamily.getBytes(),col.getBytes());
+        Result result = table.get(get);
+        table.close();
+        return new String(result.getValue(colFamily.getBytes(),col==null?null:col.getBytes()));
+    }
+
 
 
 }
--- src/test/java/com/chjzzy/imagehandle/ImageHandleApplicationTests.java
+++ src/test/java/com/chjzzy/imagehandle/ImageHandleApplicationTests.java
@@ -3,6 +3,7 @@ package com.chjzzy.imagehandle;
 import com.chjzzy.imagehandle.model.ImageModel;
 import com.chjzzy.imagehandle.response.BusinessException;
 import com.chjzzy.imagehandle.util.HadoopUtil;
+import com.chjzzy.imagehandle.util.HbaseUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,8 @@ import java.io.IOException;
 class ImageHandleApplicationTests {
     @Autowired
     private HadoopUtil hadoopUtil;
+    @Autowired
+    private HbaseUtil hbaseUtil;
     @Test
     void hadoopTest() throws IOException, BusinessException {
         ImageModel imageModel =hadoopUtil.getImageModel("image-data/bossbase图片库/1.bmp");
@@ -26,8 +29,13 @@ class ImageHandleApplicationTests {
     }
 
     @Test
-    void hbaseTest() throws IOException, BusinessException {
+    void hbaseTest() throws IOException {
+//        hbaseUtil.insertData("image","image111","info","bytecode","hahahaha");
+//        hbaseUtil.insertData("image","image111","statistic","2","100");
+        System.out.println(hbaseUtil.getData("image","image111","info","bytecode"));
+    }
+    @Test
+    void mapreduceTest(){
 
     }
-
 }
